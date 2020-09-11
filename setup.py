4cf7c51358b6be7367835c71891db9739244edc2
Neil Wu <neilwu@umich.edu>
2020-09-11 09:09:03 -0400

    added long_description read from README

--- setup.py
+++ setup.py
@@ -1,34 +1,21 @@
 from setuptools import setup
 import re
+from os import path
 
 __version__ = re.findall(
     r"""__version__ = ["']+([0-9\.]*)["']+""",
     open('pyxdsm/__init__.py').read(),
 )[0]
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(name='pyXDSM',
       version=__version__,
       description="Python script to generate PDF XDSM diagrams using TikZ and LaTeX",
-      long_description="""A python library for generating publication quality PDF XDSM diagrams.
-    This library is a thin wrapper that uses the TikZ library and LaTeX to build the PDFs.
-
-     ## What is XDSM?
-    The eXtended Design Struture Matrix (XDSM) is a graphical language for describing the movement of data and  the execution sequence for a  multidisciplinary optimization  problem.
-    You can read the [paper by Lambe and Martins](http://mdolab.engin.umich.edu/content/extensions-design-structure-matrix) for all the details.
-    If you  would like a citation for XDSM, here is the bibtex for that paper:
-
-    @article {Lambe2012,  
-        title = {Extensions to the Design Structure Matrix for the Description of Multidisciplinary Design, Analysis, and Optimization Processes},   
-        journal = {Structural and Multidisciplinary Optimization},  
-        volume = {46},  
-        year = {2012},  
-        pages = {273-284},  
-        doi = {10.1007/s00158-012-0763-y},  
-        author = {Andrew B. Lambe and Joaquim R. R. A. Martins}
-    }
-
-      """,
-    long_description_content_type="text/markdown",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       keywords='optimization multidisciplinary multi-disciplinary analysis n2 xdsm',
       author='',
       author_email='',
