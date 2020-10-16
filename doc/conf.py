68d18f7f67b397373c5d084091df8b8117f4b44f
Marco Mangano <mmangano@umich.edu>
2020-10-16 11:23:07 +0200

    generated docs with sphinx-quistart and edited conf.py

---
+++ doc/conf.py
@@ -0,0 +1,35 @@
+from sphinx_mdolab_theme.config import (
+    copyright,
+    exclude_patterns,
+    master_doc,
+    html_static_path,
+    html_context,
+    html_theme,
+    extensions,
+)
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'pyXDSM'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+# extensions.extend = ([])
+
+# mock import for autodoc
+autodoc_mock_imports = ["numpy", "mpi4py"]
---
+++ doc/index.rst
@@ -0,0 +1,20 @@
+.. pyXDSM documentation master file, created by
+   sphinx-quickstart on Fri Oct 16 11:16:12 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to pyXDSM's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
